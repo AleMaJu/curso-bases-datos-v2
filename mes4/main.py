3695e8051243f17d6e07fb942398ff7fb1758388
AleMaJu <macedojulca@gmail.com>
2026-07-26 18:26:59 +0200

    Completa Día 1: ejercicios con parámetros de ruta y consulta

--- mes4/main.py
+++ mes4/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Mi primera API")
 
@@ -13,3 +14,40 @@ def listar_alumnos():
         {"id": 1, "nombre": "Carlos", "edad": 22},
         {"id": 2, "nombre": "Laura", "edad": 25}
     ]
+@app.get("/saludo/{nombre}") # La ruta del URL donde se ejecuta: http://127.0.0.1:8000/saludo/nombre (dependerá del varlor de 'nombre')
+def saludo_personalizado(nombre: str):
+    return {"mensaje": f"¡Hola, {nombre}! ¡Bienvenido a FastAPI!"}
+
+@app.get("/suma/{a}/{b}") # La ruta del URL donde se ejecuta: http://127.0.0.1:8000/suma/a/b (dependerá de los varlores de 'a' y 'b')
+def suma(a: int,b: int):
+    return {"operacion":"suma","a":a,"b":b,"resultado":a+b,"mensaje":f"La suma entre {a} y {b} es: {a+b}."}
+
+@app.get("/buscar")
+def buscar_alumnos(nombre: str = None,edad_minima: int = 0):
+
+    alumnos = [
+        {"id": 1, "nombre": "Carlos", "edad": 22},
+        {"id": 2, "nombre": "Laura", "edad": 25},
+        {"id": 3, "nombre": "Pedro", "edad": 18}
+    ]
+
+    if nombre:
+        alumnos = [a for a in alumnos if nombre.lower() in a["nombre"].lower()]
+    if edad_minima > 0:
+        alumnos = [a for a in alumnos if a["edad"] >= edad_minima]
+
+    return {"resultados":alumnos}
+
+class AlumnoCreate(BaseModel):
+    nombre: str
+    edad: int
+
+@app.post("/alumnos")
+def crear_alumno(alumno: AlumnoCreate):
+    nuevo_alumno = {
+        "id": 100,  # Simulado
+        "nombre": alumno.nombre,
+        "edad": alumno.edad
+    }
+    return {"mensaje":"Alumno creado","alumno":nuevo_alumno}
+
